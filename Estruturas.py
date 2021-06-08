88d256fbc1dd6029713b59b6f58b7bd8261e5ffe
Alvaro Gabriel <agsa0108@gmail.com>
2021-06-08 14:11:39 -0300

    adição de funções
    # funções
    # Produto vetorial
    # Prjeção vetro em vetor
    # Normalização
    
    # Sobre objeto
    # Vetor diretor
    # Projeção vetor em reta
    # eParalelo

--- Estruturas.py
+++ Estruturas.py
@@ -1,8 +1,20 @@
+from math import sqrt, acos
+
 # Miniprojeto 0 - Processamento Gráfico 2020.2 CIn UFPE
 # Jessica Oliveira (jrmo@cin.ufpe.br)
-# Atualizado em 07/06/2021
+# Atualizado em 08/06/2021
 
 # Estruturas básicas
+
+# funções
+# Produto vetorial
+# Prjeção vetro em vetor
+# Normalização
+
+# Sobre objeto
+# Vetor diretor
+# Projeção vetor em reta
+# eParalelo
 
 class Ponto:
     def __init__(self, x1, x2, x3):
@@ -35,7 +47,7 @@ class Ponto:
 # pontoA.print()
 
 class Vetor(Ponto):
-    def __init__(self, x1, x2, x3):
+     def __init__(self, x1, x2, x3):
         self.x1 = x1
         self.x2 = x2
         self.x3 = x3
@@ -49,15 +61,48 @@ class Vetor(Ponto):
 # print(vetor1.get())
 # vetor1.print()
 
+def produtoVetorial(vetor1, vetor2):
+    i = (vetor1.x2 * vetor2.x3) - (vetor1.x3 * vetor2.x2)
+    j = (vetor1.x3 * vetor2.x1) - (vetor1.x1 * vetor2.x3)
+    k = (vetor1.x1 * vetor2.x2) - (vetor1.x2 * vetor2.x1)
+    return Vetor(i, j, k)
+
+#vetor1 = Vetor(1,1,-1)
+#vetor2 = Vetor(2,1,4)
+#vetor3 = produtoVetorial(vetor1, vetor2)
+#print(vetor3.get())
+
+# Projeção de um vetor em outro
+def projecao(vetor1, vetor2):
+    n1    = produtoEscalar(vetor1, vetor2)
+    n2    = produtoEscalar(vetor2, vetor2)
+    proj  = n1/n2
+    vetor = Vetor(proj*(vetor2.x1), proj*(vetor2.x2), proj*vetor2.x3)
+    return vetor
+
+#vetor1 = Vetor(2,3,4)
+#vetor2 = Vetor(1,-1,0)
+#print(projecao(vetor1, vetor2))
+
+# Faz com que a soma das cordenadas de um vetor seja igual a 1
+def normalizacao(vetor):
+    n1 = norma(vetor)
+    return Vetor(vetor.x1/n1, vetor.x2/n1, vetor.x2/n1)
+
+#vetor = Vetor(2,3,4)
+#soma = int(normalizacao(vetor).x1 + normalizacao(vetor).x2 + normalizacao(vetor).x3)
+#print(soma)
+
+
 class Segmento:
     def __init__(self, ponto1, ponto2):
         self.ponto1 = ponto1
         self.ponto2 = ponto2
 
     def pontoInicial(self):
-        return ponto1
+        return self.ponto1
     def pontoFinal(self):
-        return ponto2
+        return self.ponto2
     def vetorDiretor(self):
         p = (self.ponto2 - self.ponto1)
         return Vetor(p.x(),p.y(),p.z())
@@ -94,11 +139,40 @@ class Reta:
             forma = forma
         return forma
 
-# ponto = Ponto(1,2,3)
-# vetorDiretor = Vetor(4,-5,-7)
-# reta = Reta(ponto,vetorDiretor)
-# print(reta.equacaoParametrica())
+    #Adicionado para calculo de vetor diretor
+    def getVetorDiretor(self):
+        return self.vetorDiretor
 
+#ponto = Ponto(1,2,3)
+#vetorD = Vetor(4,-5,-7)
+#reta = Reta(ponto,vetorD)
+#print(reta.equacaoParametrica())
+
+def vetorDiretor(reta):
+    v = reta.getVetorDiretor()
+    return Vetor(v.x(), v.y(), v.z())
+
+#ponto = Ponto(1,2,3)
+#vetorD = Vetor(0,3,-4)
+#reta = Reta(ponto, vetorD)
+#print(vetorDiretor(reta))
+'''
+# Projeção vetor reta
+def projecao(vetor, reta):
+    vetor2 = reta.getVetorDiretor()
+    n1     = produtoEscalar(vetor, vetor2)
+    n2     = produtoEscalar(vetor2, vetor2)
+    proj   = n1/n2
+    vetor  = Vetor(proj*(vetor2.x1), proj*(vetor2.x2), proj*vetor2.x3)
+    return vetor
+
+vetor = Vetor(2,3,4)
+ponto = Ponto(1,2,3)
+vetorD = Vetor(0,3,-4)
+reta = Reta(ponto, vetorD)
+print(projecao(vetor, reta))
+
+'''
 class Esfera:
     origem = Ponto(0,0,0)
     def __init__(self, centro, raio):
@@ -151,4 +225,27 @@ class Base:
 # v3=Vetor(0,4,-1)
 # baseB = Base(v1,v2,v3)
 # print(baseB)
+def norma(vetor):
+    x1 = vetor.x1
+    x2 = vetor.x2
+    x3 = vetor.x3
 
+    return sqrt(x1**2 + x2**2 + x3**2)
+
+def eParalelo(vetor, reta):
+    vetorD = reta.getVetorDiretor()
+    if norma(vetor) == 0 or norma(vetorD) == 0:
+        return True
+    else:
+        k = vetor.x1 / vetorD.x1
+        if (vetor.x2 / vetorD.x2) != k:
+            return False
+        elif (vetor.x3 / vetorD.x3) != k:
+            return False
+        return True
+
+#vetor = Vetor(-2,3,4)
+#ponto = Ponto(1,2,2)
+#vetorD = Vetor(-4,6,8)
+#reta = Reta(ponto,vetorD)
+#print(eParalelo(vetor, reta))
