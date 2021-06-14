1e02397196d0962155777500655690079e83abad
jessica-rmo <jrmo@cin.ufpe.br>
2021-06-14 01:24:30 -0300

    intersecao(reta1,reta2)

--- fun_jrmo.py
+++ fun_jrmo.py
@@ -16,45 +16,6 @@ def formaCartesiana(plano):
 # print(formaCartesiana(p))
 
 
-def formaCartesiana(reta):
-    a = reta.ponto.x()
-    b = reta.ponto.y()
-    c = reta.ponto.z()
-    x0 = reta.vetorDiretor.x()
-    y0 = reta.vetorDiretor.y()
-    z0 = reta.vetorDiretor.z()
-
-    if (x0 == 0): x0 = 1
-    if (y0 == 0): y0 = 1
-    if (z0 == 0): z0 = 1
-
-    return [[y0/x0, -1,  0, b-((a*y0)/x0)],
-            [z0/x0,  0, -1, c-((a*z0)/x0)] ]
-
-# ponto = Ponto(-1,-2,1)
-# vetorDiretor = Vetor(-5,13,-8)
-# reta = Reta(ponto,vetorDiretor)
-# print(reta.equacaoParametrica())
-# print(formaCartesiana(reta))
-
-
-# def intersecao(reta1, reta2):
-#     ponto1 = reta1.ponto
-#     ponto2 = reta2.ponto
-#
-#     return 0
-
-# ponto = Ponto(1, 2, 3)
-# vetorDiretor = Vetor(2,4,6)
-# reta = Reta(ponto,vetorDiretor)
-# print(reta.equacaoParametrica())
-#
-# ponto2 = Ponto(2.4, 4.8, 7.2)
-# vetorDiretor = Vetor(-1,3,5)
-# reta = Reta(ponto2,vetorDiretor)
-# print(reta.equacaoParametrica())
-
-
 def intersecao(reta, plano):
     coef = formaCartesiana(plano)
     a, b, c, d = coef[0], coef[1], coef[2], coef[3]
@@ -69,7 +30,7 @@ def intersecao(reta, plano):
         if ( ((a*x)+(b*y)+(c*z)+d) == 0 ):
             return reta
         else:
-            return Null
+            return None
     else:
         t = -((produtoEscalar(reta.ponto,plano.vetorNormal)+d) / prodvRetaPlano)
         xp = x + (x0*t)
@@ -81,3 +42,67 @@ def intersecao(reta, plano):
 # reta = Reta(Ponto(1,3.3,3.38),Vetor(5.2,4,2.4))
 # inter_retaPlano = intersecao(reta,plano)
 # print(inter_retaPlano)
+
+
+def formaCartesiana(reta):
+    a = reta.ponto.x()
+    b = reta.ponto.y()
+    c = reta.ponto.z()
+    x0 = reta.vetorDiretor.x()
+    y0 = reta.vetorDiretor.y()
+    z0 = reta.vetorDiretor.z()
+
+    if (x0 == 0): x0 = 1
+    if (y0 == 0): y0 = 1
+    if (z0 == 0): z0 = 1
+
+    return [[round(y0/x0,2), -1,  0, round(b-((a*y0)/x0),2)],
+            [round(z0/x0,2),  0, -1, round(c-((a*z0)/x0),2)] ]
+
+# ponto = Ponto(2,-5.2,3.2)
+# vetorDiretor = Vetor(-5,13,-8)
+# reta = Reta(ponto,vetorDiretor)
+# print(formaCartesiana(reta))
+
+
+def intersecao(reta1, reta2):
+    A = reta1.ponto
+    vdA = reta1.getVetorDiretor()
+    vdB = reta2.getVetorDiretor()
+
+    eqB = formaCartesiana(reta2)  # [[a,b,c,d],[e,f,g,h]]
+    print(str(reta2.getVetorDiretor()))
+    a, b, c, d = eqB[0][0], eqB[0][1], eqB[0][2], eqB[0][3]
+    e, f, g, h = eqB[1][0], eqB[1][1], eqB[1][2], eqB[1][3]
+    print(str(eqB))
+    x, y, z = A.x(), A.y(), A.z()
+    # vetores da eqs da forma cartesiana da reta2
+    v1B = Vetor(a,b,c)
+    v2B = Vetor(e,f,g)
+
+    # t1 e t2 das eqs da forma castesiana da reta2 com ponto A da reta1
+    t1 = -round((produtoEscalar(A,v1B) + d)/(produtoEscalar(vdA,v1B)),2)
+    t2 = -round((produtoEscalar(A,v2B) + h)/(produtoEscalar(vdA,v2B)),2)
+
+    if ( t1 == t2 ): # existe intersecao
+        if (saoParalelos(vdA,vdB)): # vetores diretores das 2 retas
+            return reta1 #ou reta2
+        else: #retornar ponto da intersecao
+            p = x + vdA.x()*t2
+            q = y + vdA.y()*t2
+            r = z + vdA.z()*t2
+            return Ponto(round(p,2),round(q,2),round(r,2))
+    else: # nao existe intersecao
+        return None
+
+# ponto = Ponto(1, 2, 3)
+# vetorDiretor = Vetor(2,4,6)
+# reta1 = Reta(ponto,vetorDiretor)
+# ponto2 = Ponto(-1,3,5)
+# vetorDiretor = Vetor(3.4,1.8,2.2)
+# reta2 = Reta(ponto2,vetorDiretor)
+#
+# inter = intersecao(reta1,reta2)
+# print(str(inter))
+
+
